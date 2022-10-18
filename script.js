82b11a265e78be1337351cbd751d2c2ebe6be168
Arijit Paul <arijitpaul062@gmail.com>
2022-10-18 22:54:38 +0530

    Version 2.0.0

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let x = setInterval(() => {
 
     var sec = Math.floor((diff % (1000 * 60)) / (1000))
 
-    console.log(days, hours, min, sec);
+    // console.log(days, hours, min, sec);
     document.getElementById('days').innerHTML = days
     document.getElementById('hours').innerHTML = hours
     document.getElementById('min').innerHTML = min
@@ -26,4 +26,37 @@ document.getElementById("btn").addEventListener("click",function(){
 })
 
 
+let a = setInterval(() => {
+    let leftDest = new Date('Mon Nov 7 2000 00:00:01').getTime();
+    let leftNow = new Date().getTime();
 
+    let leftDiff =  leftNow- leftDest;
+    let leftYear = Math.floor(leftDiff / (1000 * 60 * 60 * 24 * 365))
+    let leftMonth = Math.floor(leftDiff / (1000 * 60 * 60 * 24 * 30))
+    let leftWeek = Math.floor(leftDiff / (1000 * 60 * 60 * 24 *7))
+
+    let leftDays = Math.floor(leftDiff / (1000 * 60 * 60 * 24))
+
+    let leftHours = Math.floor((leftDiff / (1000 * 60 * 60 )))
+    let leftMin = Math.floor((leftDiff / (1000 * 60 * 60)))
+
+    var leftSec = Math.floor((leftDiff / (1000)))
+
+    // console.log(leftDays, leftHours, leftMin, leftSec);
+    document.getElementById('leftMonth').innerHTML = leftMonth + " Months"
+
+    document.getElementById('leftWeek').innerHTML = leftWeek + " Weeks"
+
+    document.getElementById('leftDays').innerHTML = leftDays + " Days "
+    document.getElementById('leftHours').innerHTML = leftHours + " Hours"
+    document.getElementById('leftMin').innerHTML = leftMin + " Mintues"
+    document.getElementById('leftSec').innerHTML = leftSec + " Seconds"
+    document.getElementById('leftYTD').innerHTML =  leftYear + " Years"
+
+
+
+
+}, 1000);
+
+
+// 3600 *1000 = 3600000 /1000 
