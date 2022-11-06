3b2a77c1caf29b05b805ceef02a3774514801ae8
Arijit Paul <arijitpaul062@gmail.com>
2022-11-07 00:07:22 +0530

    final commit

--- script.js
+++ script.js
@@ -34,34 +34,34 @@ document.getElementById("btn").addEventListener("click", function () {
 })
 
 
-let a = setInterval(() => {
-    let leftDest = new Date('Mon Nov 7 2000 00:00:01').getTime();
-    let leftNow = new Date().getTime();
+// let a = setInterval(() => {
+//     let leftDest = new Date('Mon Nov 7 2000 00:00:01').getTime();
+//     let leftNow = new Date().getTime();
 
-    let leftDiff = leftNow - leftDest;
-    let leftYear = Math.floor(leftDiff / (1000 * 60 * 60 * 24 * 366))
-    let leftMonth = Math.floor(leftDiff / (1000 * 60 * 60 * 24 * 30))
-    let leftWeek = Math.floor(leftDiff / (1000 * 60 * 60 * 24 * 7))
+//     let leftDiff = leftDest- leftNow -;
+//     let leftYear = Math.floor(leftDiff / (1000 * 60 * 60 * 24 * 366))
+//     let leftMonth = Math.floor(leftDiff / (1000 * 60 * 60 * 24 * 30))
+//     let leftWeek = Math.floor(leftDiff / (1000 * 60 * 60 * 24 * 7))
 
-    let leftDays = Math.floor(leftDiff / (1000 * 60 * 60 * 24))
+//     let leftDays = Math.floor(leftDiff / (1000 * 60 * 60 * 24))
 
-    let leftHours = Math.floor((leftDiff / (1000 * 60 * 60)))
-    let leftMin = Math.floor((leftDiff / (1000 * 60)))
+//     let leftHours = Math.floor((leftDiff / (1000 * 60 * 60)))
+//     let leftMin = Math.floor((leftDiff / (1000 * 60)))
 
-    var leftSec = Math.floor((leftDiff / (1000)))
+//     var leftSec = Math.floor((leftDiff / (1000)))
 
-    document.getElementById('leftMonth').innerHTML = leftMonth + " Months"
-    document.getElementById('leftWeek').innerHTML = leftWeek + " Weeks"
-    document.getElementById('leftDays').innerHTML = leftDays + " Days "
-    document.getElementById('leftHours').innerHTML = leftHours + " Hours"
-    document.getElementById('leftMin').innerHTML = leftMin + " Mintues"
-    document.getElementById('leftSec').innerHTML = leftSec + " Seconds"
-    document.getElementById('leftYTD').innerHTML = leftYear + " Years"
-
-
+//     document.getElementById('leftMonth').innerHTML = leftMonth + " Months"
+//     document.getElementById('leftWeek').innerHTML = leftWeek + " Weeks"
+//     document.getElementById('leftDays').innerHTML = leftDays + " Days "
+//     document.getElementById('leftHours').innerHTML = leftHours + " Hours"
+//     document.getElementById('leftMin').innerHTML = leftMin + " Mintues"
+//     document.getElementById('leftSec').innerHTML = leftSec + " Seconds"
+//     document.getElementById('leftYTD').innerHTML = leftYear + " Years"
 
 
-}, 1000);
+
+
+// }, 1000);
 
 // ----------------------------------------
 
